e9e9b4ff07a1d7f992354a6ec8435050b6dc6f78
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-08 10:21:29 -0300

    terminado

--- components/Formularioo.jsx
+++ components/Formularioo.jsx
@@ -1,19 +1,38 @@
 import React,{useState} from "react";
 import  {StyleSheet, Button, View, Text, Alert,TextInput} from 'react-native';
 
-export const Formulario=() =>{
-    const [text, onChargeText] = useState(null);
-    return(
-        <View>
-            <View style={Styles.cuadrado}>
-                <TextInput style={Styles.input} onChargeText={onChargeText} value={text} placeholder="Ingrese texto"/>
-                <Button title="Press me" color="#f194ff" onPress={() => Alert.alert('button with adjusted color pressed')}></Button>
-                <Text>{text}</Text>
-            </View>
+export const Formulario = () => {
+    const [text, onChangeText] = useState('');
+    const [showText, setShowText] = useState(false);
+  
+    const handleButtonPress = () => {
+      setShowText(true);
+    };
+  
+    const handleTextChange = (newText) => {
+      onChangeText(newText);
+      setShowText(false);
+    };
+  
+    return (
+      <View>
+        <View style={Styles.cuadrado}>
+          <TextInput
+            style={Styles.input}
+            onChangeText={handleTextChange}
+            value={text}
+            placeholder="Ingrese texto"
+          />
+          <Button
+            title="Press me"
+            color="#f194ff"
+            onPress={handleButtonPress}
+          />
+          {showText && <Text>{text}</Text>}
         </View>
-        
-    )
-}
+      </View>
+    );
+  };
 const Styles = StyleSheet.create({
     cuadrado:{
         height:150,
